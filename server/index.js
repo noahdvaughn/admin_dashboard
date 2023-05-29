2a5fa22d8eeecd06ca90b67cc64b713b8d233841
Noah Vaughn <noahdvaughn@gmail.com>
2023-05-29 17:38:39 -0400

    created routes folder

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import helmet from 'helmet'
 import morgan from 'morgan'
+import clientRoutes from './routes/client.js'
+import generalRoutes from './routes/general.js'
+import managementRoutes from './routes/management.js'
+import salesRoutes from './routes/sales.js'
 
 dotenv.config()
 const app = express()
@@ -15,3 +19,9 @@ app.use(morgan('common'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
+
+// Routes
+app.use('/client', clientRoutes)
+app.use('/general', generalRoutes)
+app.use('/management', managementRoutes)
+app.use('/sales', salesRoutes)
