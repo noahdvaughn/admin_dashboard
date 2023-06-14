9225938d35aaa03d1b57e08e2eac794cff8dd4a5
Noah Vaughn <noahdvaughn@gmail.com>
2023-06-14 17:20:57 -0400

    added navbar search icons

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,10 +4,10 @@ import FlexBetween from './FlexBetween'
 import { useDispatch } from 'react-redux'
 import { setMode } from 'state'
 import profileImage from 'assets/pfp2.jpeg'
-import { AppBar, Toolbar, useTheme } from '@mui/material'
+import { AppBar, IconButton, InputBase, Toolbar, useTheme } from '@mui/material'
 
 const Navbar = () => {
-  const dispath = useDispatch()
+  const dispatch = useDispatch()
   const theme = useTheme()
   return (
     <AppBar
@@ -18,7 +18,36 @@ const Navbar = () => {
     }}
     >
       <Toolbar sx={{ justifyContent: "space-between"}}>
+
         {/* Leftside */}
+
+        <FlexBetween>
+          <IconButton onClick={()=> console.log('open/close sidebar')}>
+            <MenuIcon/>
+          </IconButton>
+          <FlexBetween backgroundColor={theme.palette.background.alt} borderRadius="9px" gap="3rem" p="0.1rem 1.5rem">
+          <InputBase placeholder='Search...'/>
+          <IconButton>
+            <Search />
+          </IconButton>
+          </FlexBetween>
+        </FlexBetween>
+
+        {/* RIGHT SIDE */}
+        <FlexBetween gap="1.5rem">
+          <IconButton onClick={()=> dispatch(setMode())}>
+          {theme.palette.mode === 'dark' ? (
+            <DarkModeOutlined sx={{ fontSize: "25px"}} />
+          ) : (
+            <LightModeOutlined sx={{fontSize: "25px"}} />
+          )
+        }
+          </IconButton>
+          <IconButton>
+            <SettingsOutlined sx={{fontSize: "25px"}} />
+          </IconButton>
+        </FlexBetween>
+
       </Toolbar>
     </AppBar>
   )
