6aa0d668307a4decc921bfe80dd7dc83f83fcbc1
Noah Vaughn <noahdvaughn@gmail.com>
2023-06-15 16:40:13 -0400

    added sidebar persistent drawer

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { Box, Divider, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography, useTheme } from '@mui/material'
 import { SettingsOutlined, ChevronLeft, ChevronRightOutlined, HomeOutlined, ShoppingCartOutlined, Groups2Outlined, ReceiptLongOutlined, PublicOutlined, PointOfSaleOutlined, TodayOutlined, CalendarMonthOutlined, AdminPanelSettingsOutlined, TrendingUpOutlined, PieChartOutlined } from '@mui/icons-material'
+import FlexBetween from './FlexBetween'
+import profileImage from "assets/pfp2.jpeg"
 
-const Sidebar = () => {
+const Sidebar = ({
+  drawerWidth, iseSidebarOpen, setIsSidebarOpen, isNonMobile
+}) => {
+  const {pathname} = useLocation()
+  const [active, setActive] = useState("")
+  const navigate = useNavigate()
+  const theme = useTheme()
+
+  useEffect(()=> {
+    setActive(pathname.substring(1))
+  }, [pathname])
+  
   return (
-    <div>Sidebar</div>
+    <Box component="nav">
+      
+    </Box>
   )
 }
 
