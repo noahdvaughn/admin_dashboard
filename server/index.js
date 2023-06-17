2ec8d06b142c1bd4eebf755606875474a6e1b095
Noah Vaughn <noahdvaughn@gmail.com>
2023-06-17 14:05:06 -0400

    inserted data to atlas

--- server/index.js
+++ server/index.js
@@ -10,6 +10,10 @@ import generalRoutes from './routes/general.js'
 import managementRoutes from './routes/management.js'
 import salesRoutes from './routes/sales.js'
 
+//data imports
+import User from './models/User.js'
+import { dataUser } from './data/index.js'
+
 dotenv.config()
 const app = express()
 app.use(express.json())
@@ -35,5 +39,8 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+
+    //only add data once
+    // User.insertMany(dataUser)
   })
   .catch((error) => console.log(`${error}, did not connect`))
