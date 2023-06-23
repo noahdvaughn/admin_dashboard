7940a04ba622f9b095f75fb25ae46efa23af2236
Noah Vaughn <noahdvaughn@gmail.com>
2023-06-22 22:44:31 -0400

    finished performance page

--- server/controllers/management.js
+++ server/controllers/management.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose'
 import User from '../models/User.js'
 import Transaction from '../models/Transaction.js'
 
@@ -9,27 +10,30 @@ export const getAdmins = async (req, res) => {
     res.status(404).json({ message: error.message })
   }
 }
+
 export const getUserPerformance = async (req, res) => {
   try {
     const { id } = req.params
+
     const userWithStats = await User.aggregate([
       { $match: { _id: new mongoose.Types.ObjectId(id) } },
       {
         $lookup: {
           from: 'affiliatestats',
           localField: '_id',
-          foreignField: 'user',
+          foreignField: 'userId',
           as: 'affiliateStats'
         }
       },
       { $unwind: '$affiliateStats' }
     ])
+
     const saleTransactions = await Promise.all(
       userWithStats[0].affiliateStats.affiliateSales.map((id) => {
         return Transaction.findById(id)
       })
     )
-    const filteredSaleTransactions = salesTransactions.filter(
+    const filteredSaleTransactions = saleTransactions.filter(
       (transaction) => transaction !== null
     )
 
